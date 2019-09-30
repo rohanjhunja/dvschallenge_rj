ca39c63c8bd3319086f84a78a3990f8b323118c7
rohanjhunja <54926642+rohanjhunja@users.noreply.github.com>
2019-09-30 16:28:53 +0530

    Update myChart.js

--- myChart.js
+++ myChart.js
@@ -13,7 +13,16 @@ function renderChart(data, labels) {
 }
 
 $("#renderBtn").click(
+    d3.csv('https://github.com/rohanjhunja/dvschallenge_rj/blob/master/test.csv')
+  .then(makeChart);
+
+function makeChart(players) {
+  // players is an array of objects where each object represents a player
+}
+    
+    
     function () {
+        
         data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
         labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
         renderChart(data, labels);
